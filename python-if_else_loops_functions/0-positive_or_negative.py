69aecb6c6f4aac9fe2d02ae2952fc03535df3fb4
Shivani <shivanil@brightcomgroup.com>
2023-01-01 21:08:11 -0800

    0-positive_or_negative.py

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if number > 0:
+    print("{} is positive".format(number))
+elif number == 0:
+    print("{} is Zero".format(number))
+else:
+    print("{} is negative".format(number))
