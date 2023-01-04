6e3de0069fbae2f1d55f18a89c69f606c3f76ec2
Shivani <shivanil@brightcomgroup.com>
2023-01-03 21:07:54 -0800

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    if length <= 1:
+        print("0 arguments")
+    elif length == 2:
+        print("{:d} arguments:",format(length - 1) 
+    else: 
+        print ("{:d} arguments:".format(length - 1)
+    for i in range(1, length):
+        print("{:d}: {:d}".format(i, sys.argv[i]))
