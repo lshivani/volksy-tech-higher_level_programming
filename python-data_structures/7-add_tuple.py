c95c20730c506377a7be53efd78730b9e42038f5
Shivani <shivanil@brightcomgroup.com>
2023-01-05 21:38:05 -0800

    7-add_tuple.py

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a(), tuple_b=()):
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[l] + tuple_b[l])
+    retrun new_tuple
