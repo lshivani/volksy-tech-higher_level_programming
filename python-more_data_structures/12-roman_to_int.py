26ce1f29567a7a3d903ec4fc0f8afb4b5947f599
Shivani <shivanil@brightcomgroup.com>
2023-01-08 22:31:07 -0800

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,6 @@
 # 12-roman_to_int.py
 # Brennan D Baraban <375@holbertonschool.com>
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
