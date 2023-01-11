0a0e4b218c5295470391b0e1fc701e19b54a3131
Shivani <shivanil@brightcomgroup.com>
2023-01-10 20:31:20 -0800

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 2-Square """
+
+class Square:
+    """ Size = 0 """
+    def __init__(self, size = 0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            return ValueError("size must be >= 0")
+        self.__size = size
