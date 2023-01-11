d3c757cd542843d6a2c1f408761792fefc33ae12
Shivani <shivanil@brightcomgroup.com>
2023-01-11 02:48:46 -0800

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,15 +7,15 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
+    def area(self):
+        return self.__size * self.__size
+
+    def size(self):
+        return self.__size
+
     def size(self, value):
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             return ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        return self.__size * self.__size
-
-    def size(self):
-        return self.__size
