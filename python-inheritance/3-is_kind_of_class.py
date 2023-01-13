047658236ac5c76e00065c400e0666bf85e921fa
Shivani <shivanil@brightcomgroup.com>
2023-01-13 01:04:30 -0800

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ True or False"""
+    return isinstance(obj, a_class)
---
+++ python-inheritance/33-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" kind of classes """
+
+
+def is_kind_of_class(obj, a_class):
+    """ true or false"""
+    return isinstance(obj, a_class)
