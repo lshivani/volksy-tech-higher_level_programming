283cecb28a2ac518bb81a7d805cd44e02b5940ff
Shivani <shivanil@brightcomgroup.com>
2023-01-13 01:26:51 -0800

    7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" creates a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """An class named BaseGeomety """
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates input"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
