b9c6677697c03d04b7d138ddfde9e7f7f71db7d6
Shivani <shivanil@brightcomgroup.com>
2023-01-13 01:58:52 -0800

    1-my_list.txt

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"class named MyList"
+
+
+class MyList(list):
+    """A class named MyList """"
+    def print_sorted(self):
+        """Prints instance"""
+        print(sorted(self))
