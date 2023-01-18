79952bde5b727fdec8413808efac96cdb586f86a
Shivani <shivanil@brightcomgroup.com>
2023-01-18 03:40:49 -0800

    4-from_json_string.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ save json """
-    with open(filename, 'w', encoding='utf-8'):
-        return file.write(json.dumps(my_obj))
+    with open(filename, 'w', encoding='utf-8')as file:
+        file.write(json.dumps(my_obj))
