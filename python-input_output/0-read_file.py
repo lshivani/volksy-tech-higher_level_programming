d9ffeefda552c42e023658ed6b25a8e51ff86811
Shivani <shivanil@brightcomgroup.com>
2023-01-18 01:02:17 -0800

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def readfile(filename=""):
     """ reads the file and prints """
     with open(filename, encoding='utf-8')as file:
         for line in file:
-            print(line, end="")
+            print(line, end="")
