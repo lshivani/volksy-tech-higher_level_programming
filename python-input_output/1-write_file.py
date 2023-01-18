07c069c7201f9c3ec92cd3d061f929e55165573f
Shivani <shivanil@brightcomgroup.com>
2023-01-18 01:30:54 -0800

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """ write file"""
 
 
-def write_file(filename=""):
+def write_file(filename="", text=""):
     """ lines count """
     with open(filename,'w', encoding='utf-8')as file:
         return file.write(text)
