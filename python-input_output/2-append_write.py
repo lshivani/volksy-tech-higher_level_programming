559dca95203f369208773cbd8293a49dc403b67a
Shivani <shivanil@brightcomgroup.com>
2023-01-18 01:39:50 -0800

    2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" append write """
+
+
+def append_write(filename="", text=""):
+    """ no of characters """
+    with open(filename, a, encoding='utf-8')as file:
+        return file.write(text)
