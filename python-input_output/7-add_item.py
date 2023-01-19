92e42df75e7f7532167b16f72ef0146e92983e05
Shivani <shivanil@brightcomgroup.com>
2023-01-18 20:47:40 -0800

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This module creates a script that adds args to Python list and saves them"""
 
+
 from os import path
 from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
 
 if path.exists("add_item.json") is False:
     save_to_json_file([], "add_item.json")
