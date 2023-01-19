e370fff1ad6bed0ed2b766e2388c59e56995f8d9
Shivani <shivanil@brightcomgroup.com>
2023-01-18 21:02:10 -0800

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""This module creates a script that adds args to Python list and saves them"""
+"""Add all arguments to a Python list and save them to a file."""
 
 
-from os import path
-from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+import sys
 
 
-if path.exists("add_item.json") is False:
-    save_to_json_file([], "add_item.json")
-my_list = load_from_json_file("add_item.json")
-for i in range(1, len(argv)):
-    my_list.append(argv[i])
-save_to_json_file(my_list, "add_item.json")
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
