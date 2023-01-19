27975cb1b95e82fff05b26ddc77eca3900570c07
Shivani <shivanil@brightcomgroup.com>
2023-01-18 22:34:34 -0800

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,13 +6,13 @@ class Student:
     """class """
 
 
-def __init__(self, first_name, last_name, age):
-    """ initializing """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    def __init__(self, first_name, last_name, age):
+        """ initializing """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
 
-def to_json(self):
-    """ retruns data """
-    return self.__dict__
+    def to_json(self):
+        """ retruns data """
+        return self.__dict__
