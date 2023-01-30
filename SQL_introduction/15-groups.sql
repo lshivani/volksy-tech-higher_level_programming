020cb561e4e159b1de49ea799bbd3a678195e887
Shivani <shivanil@brightcomgroup.com>
2023-01-30 01:32:43 -0800

    15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list no of records with same record
-SELECT score, COUNT(score) as number FROM second_table GROUP BY socre ORDER BY number DESC;
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
