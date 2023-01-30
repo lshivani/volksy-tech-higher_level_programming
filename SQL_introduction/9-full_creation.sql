a31d8741d2828c615969b40c3858605669cd43f9
Shivani <shivanil@brightcomgroup.com>
2023-01-29 23:02:18 -0800

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,5 +2,7 @@
 CREATE TABLE IF NOT EXISTS second_table(
 	id INT DEFAULT NULL,
 	name VARCHAR(256),
-	score INT DEFAULT NULL,
+	score INT,
 );
+INSERT INTO second_table 
+VALUES(1, 'John', 10),(2, 'Alex', 3),(3, 'Bob', 14),(4, 'George', 8);
