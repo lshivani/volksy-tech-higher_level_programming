9379616769fa0189553d4b5d42293c364a9f8bf3
Shivani <shivanil@brightcomgroup.com>
2023-02-01 22:59:01 -0800

    11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,5 @@
 -- list all shows  from database
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows RIGHT JOIN tv_show_genres WHERE genre_id IS NOT NULL ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM
+tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
