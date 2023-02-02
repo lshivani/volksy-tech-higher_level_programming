6c824ddf13e13c06b4b25d23dc776a69c38f42f2
Shivani <shivanil@brightcomgroup.com>
2023-02-01 23:07:27 -0800

    12-no_genre.sql

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- show in acsending order
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows ORDER BY tv_shows.title ASC,tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows AND tv_show_genres ORDER BY tv_shows.title ASC,tv_show_genres.genre_id ASC;
