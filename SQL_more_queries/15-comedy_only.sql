15a6060cda97aecd41ad93cc077a71c554198272
Shivani <shivanil@brightcomgroup.com>
2023-02-02 01:48:20 -0800

    15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- name=comedy ,asending
-SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_show_genres.show_id = tv_genres.id WHERE tv_shows.name='Comedy'
+SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name='Comedy'
 ORDER BY tv_shows.title ASC;
 
