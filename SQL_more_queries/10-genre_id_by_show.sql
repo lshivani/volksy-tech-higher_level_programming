3a360e5df94c5f5f1a4d63abf4a6c7d13d5f5ed1
Shivani <shivanil@brightcomgroup.com>
2023-02-01 21:57:50 -0800

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- import the database dump from table to ur mysql server
 SELECT tv_shows.title, tv_show_genres.genres.id FROM
-tv_shows RIGHT JOIN tv_shows_genres WHERE tv_shows_genres.show.id=tv_show.id
+tv_shows RIGHT JOIN tv_shows_genres ON tv_shows_genres.show.id=tv_show.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
