91a6a24baa78167d353e4fdcd2c2fc674ff624c3
Shivani <shivanil@brightcomgroup.com>
2023-02-02 01:35:38 -0800

    14-my_genres.sql

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- show  one record named with dexter , ascending 
 SELECT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_show_genres_show_id = tv_shows.id WHERE tv_shows.title='Dexter'
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title='Dexter'
 ORDER BY tv_genres.name ASC;
 
