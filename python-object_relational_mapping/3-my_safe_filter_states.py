9b167dcc1e1c62e47a2c64d50baabd9982be34e4
Shivani <shivanil@brightcomgroup.com>
2023-02-07 22:13:46 -0800

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,6 +4,7 @@
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     c = conn.cursor()
