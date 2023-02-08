328b428f4af28369480e0195a1711938647b93c4
Shivani <shivanil@brightcomgroup.com>
2023-02-08 01:18:15 -0800

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     c = conn.cursor()
     c.execute("SELECT cities.name from cities" +
-              " INNER JOIN states ON cities.state_id = state.id" +
+              " INNER JOIN states ON cities.state_id = stateis.id" +
               " WHERE states.name = %s ORDER BY cities.id ASC", [sys.argv[4]])
     a = c.fetchall()
     for i in a:
-        print(",".join[0])
+        print(",".join(i[0]))
     c.close()
     conn.close()
