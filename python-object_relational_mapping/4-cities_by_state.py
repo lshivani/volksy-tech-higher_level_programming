66198305e9e1259166abb970def23256687875aa
Shivani <shivanil@brightcomgroup.com>
2023-02-07 22:58:12 -0800

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     c =conn.cursor()
     c.execute("SELECT cities.id, cities.name, states.name from cities" +
-            "INNER JOIN cities.states_id = states_id ORDER BY" +
-            "cities_id ASC")
+              "INNER JOIN cities.states_id = states_id ORDER BY" +
+              "cities_id ASC")
     a = c.fectchall()
     for i in a:
         print(i)
