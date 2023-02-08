74e2adf3111b171325eb959f11b4280c91a304fd
Shivani <shivanil@brightcomgroup.com>
2023-02-07 20:28:58 -0800

    3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" sql injection"""
+
+import sys
+import MySQLdb
+
+if __name__ == __main__:
+    conn = MySQLdb.connect(host="localhost", port="3306", user="sys.argv[1]", passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    c = conn.cursor()
+    c.execute("SELECT * FROM states WHERE name = {} ORDER BY id ASC".format(sys.argv[4])
+            a = c.fetchall()
+            for i in c:
+                print()
+            c.close()
+            conn.close()
