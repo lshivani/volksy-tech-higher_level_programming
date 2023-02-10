a72e22550037d06c55f2eb61da88c70157b618b3
Shivani <shivanil@brightcomgroup.com>
2023-02-09 20:25:27 -0800

    102-top_city.sql

---
+++ SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- the max temperature (Fahrenheit) for top 3 cities in July and August, ordered by temperature (descending)
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+WHERE month BETWEEN 7 AND 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
