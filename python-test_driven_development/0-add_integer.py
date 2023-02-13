a9f5c1493f398c4e2f0f869722cd71c8f37a46d1
Shivani <shivanil@brightcomgroup.com>
2023-02-12 21:42:00 -0800

    0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines the add integer function
+"""
+
+
+def add_integer(a, b=98):
+    """ Function that adds two"""
+    if (type(a) != int and type(a) != float) or a != a:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float or b != b:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
