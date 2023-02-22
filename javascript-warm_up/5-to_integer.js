d20ba085073fbb54c21ad92a64678b60c5763d40
lshivani <shivanil@brightcomgroup.com>
2023-02-21 23:29:18 -0800

    5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if(isNaN(parseInt(process.argv[2], 10))) {
+if (isNaN(parseInt(process.argv[2], 10))) {
   console.log('Not a number');
 } else {
   console.log(parseInt(process.argv[2], 10));
