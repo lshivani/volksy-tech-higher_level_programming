2c06b9c01155dd47da04c90cebc79829e5e950df
lshivani <shivanil@brightcomgroup.com>
2023-02-21 22:10:52 -0800

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,5 +4,5 @@ if (process.argv.length < 3) {
 } else if (process.argv.length === 3) {
 	console.log('Argument found');
 } else {
-       console.log('Arguments found')
+       console.log('Arguments found');
 }
