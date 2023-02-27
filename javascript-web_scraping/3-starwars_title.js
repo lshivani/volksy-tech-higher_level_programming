894b7b3068f9687cd8d2488984cd1b108f50e147
lshivani <shivanil@brightcomgroup.com>
2023-02-27 02:27:52 -0800

    3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-let resultDict = {};
+let url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    resultDict = JSON.parse(body);
-    console.log(resultDict.title);
-  }
+  console.log(error || JSON.parse(body).title);
 });
