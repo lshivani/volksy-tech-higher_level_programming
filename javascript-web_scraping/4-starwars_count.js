5c7abaccecd93f5bd2afb75b0e3437ee75b2b683
lshivani <shivanil@brightcomgroup.com>
2023-02-26 22:45:23 -0800

    4-starwars_count.js

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+let resultDict = {};
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    resultDict = JSON.parse(body);
+    console.log(resultDict.title);
+  }
+});#!/usr/bin/node
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+let resultDict = {};
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    resultDict = JSON.parse(body);
+    console.log(resultDict.title);
+  }
+});
+
