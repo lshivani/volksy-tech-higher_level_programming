beab06c4f5cb69c09fc864c77b1b603761d820e4
lshivani <shivanil@brightcomgroup.com>
2023-02-26 22:46:51 -0800

    4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = process.argv[2];
 let resultDict = {};
-request(url, function (error, response, body) {
+let count = 0;
+let i = 0;
+let j = 0;
+request(url, function (
+  error, response, body) {
   if (error) {
     console.log(error);
   } else {
     resultDict = JSON.parse(body);
-    console.log(resultDict.title);
-  }
-});#!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-let resultDict = {};
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    resultDict = JSON.parse(body);
-    console.log(resultDict.title);
+    for (i = 0; i < resultDict.results.length; i++) {
+      for (j = 0; j < resultDict.results[i].characters.length; j++) {
+        if (resultDict.results[i].characters[j].includes('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
 });
-
