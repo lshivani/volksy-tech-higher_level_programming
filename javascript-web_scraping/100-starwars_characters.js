d95bc894362d79281546af8fe8aaf6066564fea3
lshivani <shivanil@brightcomgroup.com>
2023-02-27 03:07:13 -0800

    100-starwars_characters.js

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+
+const filmsURL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(filmsURL, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const urlList = JSON.parse(body).characters;
+  for (const url of urlList) {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.error(error);
+      }
+      console.log(JSON.parse(body).name);
     });
   }
 });
+
