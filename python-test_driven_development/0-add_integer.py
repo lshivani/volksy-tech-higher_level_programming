bb90b09a968ea182617f10448d2ac099e2a0c04b
lshivani <shivanil@brightcomgroup.com>
2023-05-23 02:39:43 -0700

    done

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""This module defines the add integer function
+"""
+This is the "add integer" module.
+The add integer module supplies one function, add_integer().
 """
 
 
 def add_integer(a, b=98):
-    """ Function that adds two"""
+    """
+    Return the sum of a and b, two integers.
+    """
 
-    if (type(a) != int and type(a) != float) or a != a:
-        raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float or b != b:
-        raise TypeError("b must be an integer")
+    if type(a) is not int and type(a) is not float:
+        raise TypeError('a must be an integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be an integer')
     return int(a) + int(b)
